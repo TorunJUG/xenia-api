4c89da36b14c1a2926c07816b43392e5ea37b24b
Zbyszko <mephiss@gmail.com>
2014-09-06 15:02:21 +0200

    giveaway mock

--- src/main/groovy/pl/jug/torun/xenia/rest/GiveAwayController.groovy
+++ src/main/groovy/pl/jug/torun/xenia/rest/GiveAwayController.groovy
@@ -1,8 +1,11 @@
 package pl.jug.torun.xenia.rest
 
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import pl.jug.torun.xenia.rest.dto.GiveAwayRequest
 import pl.jug.torun.xenia.rest.dto.GiveAwayResponse
 import pl.jug.torun.xenia.rest.dto.GiveAwaysResponse
 
@@ -10,13 +13,17 @@ import pl.jug.torun.xenia.rest.dto.GiveAwaysResponse
  * Created by mephi_000 on 06.09.14.
  */
 @RestController
-@RequestMapping("/event/{eventId}/giveaway")
+@RequestMapping(value = "/event/{eventId}/giveaway", produces = ["application/json"])
 class GiveAwayController {
 
-    @RequestMapping(value = '/{id}', method = RequestMethod.GET, produces = ["application/json"])
+    @RequestMapping(value = '/{id}', method = RequestMethod.GET)
     GiveAwayResponse getGiveAways() {
         return new GiveAwayResponse(id: 1, prizeId: 1, amount: 2)        
     }
-    
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = ["application/json"])
+    Map putGiveAway(@PathVariable('eventId') long eventId, @RequestBody GiveAwayRequest request) {
+        return [resourceUrl: "/events/" + eventId + "/giveaway/1231"]
+    }
     
 }
