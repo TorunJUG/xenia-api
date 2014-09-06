5b693f04a5bcc0f3d944413fac10468a80e27d3f
wololock <szymon.stepniak@gmail.com>
2014-09-06 13:58:15 +0200

    Prize entity with PrizeRepository

--- src/main/groovy/pl/jug/torun/xenia/model/Prize.groovy
+++ src/main/groovy/pl/jug/torun/xenia/model/Prize.groovy
@@ -1,12 +1,29 @@
 package pl.jug.torun.xenia.model
 
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
 /**
  * Created by mephi_000 on 06.09.14.
  */
+@Entity
 class Prize {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     int id;
+
+    @Column(nullable = false)
     String name
+
+    @Column(nullable = true)
     String producer
+
+    @Column(nullable = true)
     String imageUrl
+
+    @Column(nullable = true)
     String sponsorName
 }
