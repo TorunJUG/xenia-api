cf283e4391c527cafbf1791367c43569361de2ed
Zbyszko <mephiss@gmail.com>
2014-09-06 12:59:01 +0200

    attendee Mock

--- src/main/groovy/pl/jug/torun/xenia/rest/AttendeeController.groovy
+++ src/main/groovy/pl/jug/torun/xenia/rest/AttendeeController.groovy
@@ -10,17 +10,15 @@ import pl.jug.torun.xenia.rest.dto.AttendeesResponse
 /**
  * Created by mephi_000 on 06.09.14.
  */
+
 @Controller
-@RequestMapping("/attendees")
-class AttendeesController {
-    
-    
+@RequestMapping("/attendee")
+class AttendeeController {
+
+
     @RequestMapping(method = RequestMethod.GET, produces = ["application/json"])
-    @ResponseBody AttendeesResponse getAttendees() {
-        return new AttendeesResponse(attendees: [
-                new AttendeeResponse(id: 1, displayName: 'Szymon', photoUrl: 'http://photos3.meetupstatic.com/photos/member/2/a/3/2/thumb_179350802.jpeg'),
-                new AttendeeResponse(id: 2, displayName: 'Zbyszko', photoUrl: 'http://torun.jug.pl/images/speakers/papierski-zbyszko.jpg')
-        ])
+    @ResponseBody AttendeeResponse getAttendees() {
+        new AttendeeResponse(id: 2, displayName: 'Zbyszko', photoUrl: 'http://torun.jug.pl/images/speakers/papierski-zbyszko.jpg')
     }
-     
 }
+    
