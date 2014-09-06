59617ca7136bf5b9e1eb97f5843246e8c6dd0bbe
Zbyszko <mephiss@gmail.com>
2014-09-06 12:51:02 +0200

    /attendees mock

---
+++ src/main/groovy/pl/jug/torun/xenia/rest/dto/AttendeeResponse.groovy
@@ -0,0 +1,14 @@
+package pl.jug.torun.xenia.rest.dto
+
+/**
+ * Created by mephi_000 on 06.09.14.
+ */
+class AttendeeResponse {
+    long id
+    String displayName
+    String photoUrl
+    
+    
+    
+    
+}
