d7ad72bca490cb2f3157784081d47c41d86a46e7
Zbyszko <mephiss@gmail.com>
2014-09-20 23:49:23 +0200

    fix for content-type header in cors

--- src/main/groovy/pl/jug/torun/xenia/rest/AllowMethodsFilter.groovy
+++ src/main/groovy/pl/jug/torun/xenia/rest/AllowMethodsFilter.groovy
@@ -21,7 +21,7 @@ class AllowMethodsFilter implements Filter{
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with", "Content-Type", "Accept");
         chain.doFilter(req, res);
     }
 
