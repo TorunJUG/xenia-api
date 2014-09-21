db256b9d41f53cda121fd2319e36c77262e30feb
Zbyszko <mephiss@gmail.com>
2014-09-21 12:41:46 +0200

    prizeName in giveaway

--- src/main/groovy/pl/jug/torun/xenia/rest/dto/GiveAwayResponse.groovy
+++ src/main/groovy/pl/jug/torun/xenia/rest/dto/GiveAwayResponse.groovy
@@ -9,10 +9,12 @@ class GiveAwayResponse {
     int id
     int prizeId
     int amount
+    String prizeName
     
     GiveAwayResponse(GiveAway giveAway) {
         this.id = giveAway.id
         this.prizeId = giveAway.prize.id
         this.amount = giveAway.amount
+        this.prizeName = giveAway.prize.name
     }
 }
