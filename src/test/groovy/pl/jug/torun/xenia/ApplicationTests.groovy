5b16b4321ae0725142d06b224ca6f24a2c144aea
Szymon Stepniak <szymon.stepniak@gmail.com>
2016-04-01 22:40:28 +0200

    MockServletContext configuration added

--- src/test/groovy/pl/jug/torun/xenia/ApplicationTests.groovy
+++ src/test/groovy/pl/jug/torun/xenia/ApplicationTests.groovy
@@ -2,12 +2,15 @@ package pl.jug.torun.xenia
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.mock.web.MockServletContext
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 
 @RunWith(SpringJUnit4ClassRunner)
 @SpringApplicationConfiguration(classes = Application)
+@ContextConfiguration(classes = MockServletContext)
 @WebAppConfiguration
 class ApplicationTests {
 
