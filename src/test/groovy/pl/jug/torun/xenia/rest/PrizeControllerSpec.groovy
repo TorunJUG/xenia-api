2ee48154612a7d8258a9d5c5fa1a87abc077acec
Szymon Stepniak <szymon.stepniak@gmail.com>
2016-05-14 12:01:55 +0200

    spring boot upgraded to 1.3.3

--- src/test/groovy/pl/jug/torun/xenia/rest/PrizeControllerSpec.groovy
+++ src/test/groovy/pl/jug/torun/xenia/rest/PrizeControllerSpec.groovy
@@ -1,32 +1,28 @@
 package pl.jug.torun.xenia.rest
 
-import org.hamcrest.Matchers
+import org.hamcrest.text.MatchesPattern
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.IntegrationTest
-import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.boot.test.SpringApplicationContextLoader
 import org.springframework.http.MediaType
-import org.springframework.mock.web.MockServletContext
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 import pl.jug.torun.xenia.Application
 import pl.jug.torun.xenia.dao.PrizeRepository
 import pl.jug.torun.xenia.model.Prize
-import spock.lang.Stepwise
 
 import static org.hamcrest.Matchers.*
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @RunWith(SpringJUnit4ClassRunner)
 @ContextConfiguration(loader = SpringApplicationContextLoader, classes = Application)
@@ -62,7 +58,7 @@ class PrizeControllerSpec  {
 
         //then:
         response.andExpect(status().isCreated())
-                .andExpect(jsonPath('$.resourceUrl', is(equalTo('/prize/1'))))
+                .andExpect(jsonPath('$.resourceUrl', MatchesPattern.matchesPattern("/prize/[0-9]+")))
 
         //when:
         response = request.perform(post("/prize").contentType(MediaType.APPLICATION_JSON).content(json))
@@ -76,7 +72,7 @@ class PrizeControllerSpec  {
 
         //then:
         response.andExpect(status().isCreated())
-                .andExpect(jsonPath('$.resourceUrl', is(equalTo('/prize/2'))))
+                .andExpect(jsonPath('$.resourceUrl', MatchesPattern.matchesPattern("/prize/[0-9]+")))
     }
     
     @Test
