ecdf48264f085baae51e2ff9b4177b0529ad72b1
Marcin Świerczyński <marcin@swierczynski.net>
2016-05-17 17:59:33 +0200

    Add more tests for MeetupEventsService

--- src/test/groovy/pl/jug/torun/xenia/service/MeetupEventsServiceSpec.groovy
+++ src/test/groovy/pl/jug/torun/xenia/service/MeetupEventsServiceSpec.groovy
@@ -28,12 +28,58 @@ class MeetupEventsServiceSpec extends Specification {
         service.meetupClient = meetupClient
     }
 
+    void "should create new event based on remote data"() {
+        given:
+        Member remoteMember = new Member(id: 10, displayName: 'Marcin')
+
+        Event remoteEvent = new Event(id: 1, meetupId: 2, title: 'Event 2', attendees: [])
+        meetupClient.findAllEvents() >> [remoteEvent]
+
+        eventRepository.findAll() >> []
+
+        meetupClient.findAllAttendeesOfEvent(2) >> [new MeetupMember(id: 100, member: remoteMember)]
+        meetupMemberRepository.findOne(100) >> null
+
+        meetupMemberRepository.save(new MeetupMember(id: 100, member: remoteMember))
+
+        when:
+        service.refreshEvents()
+
+        then:
+        0 * meetupMemberRepository.getByMember(_)
+        1 * eventRepository.save(remoteEvent)
+    }
+
+    void "should create new event based on remote data if different event exist"() {
+        given:
+        Member remoteMember = new Member(id: 10, displayName: 'Marcin')
+        Member localMember = new Member(id: 11, displayName: 'Marci')
+
+        Event remoteEvent = new Event(id: 1, meetupId: 2, title: 'Event 2', attendees: [])
+        meetupClient.findAllEvents() >> [remoteEvent]
+
+        Event localEvent = new Event(id: 3, meetupId: 4, title: 'Event 3', attendees: [localMember])
+        eventRepository.findAll() >> [localEvent]
+
+        meetupClient.findAllAttendeesOfEvent(2) >> [new MeetupMember(id: 100, member: remoteMember)]
+        meetupMemberRepository.findOne(100) >> null
+
+        meetupMemberRepository.save(new MeetupMember(id: 100, member: remoteMember))
+
+        when:
+        service.refreshEvents()
+
+        then:
+        0 * meetupMemberRepository.getByMember(_)
+        1 * eventRepository.save(remoteEvent)
+    }
+
     void "should update member details based on remote data"() {
         given:
         Member remoteMember = new Member(id: 10, displayName: 'Marcin')
         Member localMember = new Member(id: 10, displayName: 'Marci')
 
-        Event remoteEvent = new Event(id: 1, meetupId: 2, title: 'Event', attendees: [remoteMember])
+        Event remoteEvent = new Event(id: 1, meetupId: 2, title: 'Event', attendees: [])
         meetupClient.findAllEvents() >> [remoteEvent]
 
         Event localEvent = new Event(id: 2, meetupId: 2, title: 'Event', attendees: [localMember])
@@ -51,6 +97,33 @@ class MeetupEventsServiceSpec extends Specification {
         then:
         1 * meetupMemberRepository.save(new MeetupMember(id: 100, member: remoteMember))
         localEvent.attendees.first().displayName == 'Marcin'
+    }
+
+    void "should create new member based on remote data"() {
+        given:
+        Member remoteMember = new Member(id: 10, displayName: 'Marcin')
+        Member localMember = new Member(id: 11, displayName: 'Foo')
+
+        Event remoteEvent = new Event(id: 1, meetupId: 2, title: 'Event', attendees: [])
+        meetupClient.findAllEvents() >> [remoteEvent]
+
+        Event localEvent = new Event(id: 2, meetupId: 2, title: 'Event', attendees: [localMember])
+        eventRepository.findAll() >> [localEvent]
+
+        MeetupMember remoteMeetupMember = new MeetupMember(id: 110, member: remoteMember)
+        meetupClient.findAllAttendeesOfEvent(2) >> [remoteMeetupMember]
+        MeetupMember meetupMember = new MeetupMember(id: 100, member: localMember)
+        meetupMemberRepository.findOne(100) >> null
+
+        meetupMemberRepository.getByMember(localMember) >> meetupMember
+        meetupMemberRepository.getByMember(remoteMember) >> remoteMeetupMember
+
+        when:
+        service.refreshEvents()
+
+        then:
+        1 * meetupMemberRepository.save(remoteMeetupMember) >> remoteMeetupMember
+        localEvent.attendees.last().displayName == 'Marcin'
     }
 
     void "should remove members from local event if they are removed from remote event"() {
@@ -82,7 +155,7 @@ class MeetupEventsServiceSpec extends Specification {
         Member localMember1 = new Member(id: 10, displayName: 'Marcin')
         Member localMember2 = new Member(id: 11, displayName: 'Foo')
 
-        Event remoteEvent = new Event(id: 1, meetupId: 2, title: 'Event', attendees: [remoteMember])
+        Event remoteEvent = new Event(id: 1, meetupId: 2, title: 'Event', attendees: [])
         meetupClient.findAllEvents() >> [remoteEvent]
 
         Event localEvent = new Event(id: 2, meetupId: 2, title: 'Event', attendees: [localMember1, localMember2])
@@ -103,6 +176,30 @@ class MeetupEventsServiceSpec extends Specification {
 
         then:
         localEvent.attendees == [localMember1]
+    }
+
+    void "should not remove members from local event if they are nothing to remove"() {
+        given:
+        Member remoteMember = new Member(id: 10, displayName: 'Marcin')
+        Member localMember = new Member(id: 10, displayName: 'Marcin')
+
+        Event remoteEvent = new Event(id: 1, meetupId: 2, title: 'Event', attendees: [])
+        meetupClient.findAllEvents() >> [remoteEvent]
+
+        Event localEvent = new Event(id: 2, meetupId: 2, title: 'Event', attendees: [localMember])
+        eventRepository.findAll() >> [localEvent]
+
+        meetupClient.findAllAttendeesOfEvent(2) >> [new MeetupMember(id: 100, member: remoteMember)]
+
+        MeetupMember meetupMember = new MeetupMember(id: 100, member: localMember)
+        meetupMemberRepository.findOne(100) >> meetupMember
+        meetupMemberRepository.getByMember(localMember) >> meetupMember
+
+        when:
+        service.refreshEvents()
+
+        then:
+        localEvent.attendees == [localMember]
     }
 
 }
