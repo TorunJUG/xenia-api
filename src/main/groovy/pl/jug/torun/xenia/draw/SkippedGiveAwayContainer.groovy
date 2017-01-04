5402a369e948c3856a9ab81c97c65c2e13115ad0
Łukasz Sawicki <sawickil@gmail.com>
2017-01-05 00:44:24 +0100

    Minor improvement
    https://github.com/TorunJUG/Xenia/issues/7

--- src/main/groovy/pl/jug/torun/xenia/draw/SkippedGiveAwayContainer.groovy
+++ src/main/groovy/pl/jug/torun/xenia/draw/SkippedGiveAwayContainer.groovy
@@ -9,18 +9,15 @@ import java.util.concurrent.ConcurrentSkipListSet
 @Component
 class SkippedGiveAwayContainer {
 
-    Map<GiveAway, Set<Member>> skippedGiveWays = new ConcurrentHashMap<>()
+    private final Map<GiveAway, Set<Member>> skippedGiveWays = new ConcurrentHashMap<>()
 
     Set<Member> getMembersByGiveAway(GiveAway giveaway) {
         (skippedGiveWays[giveaway] ?: new HashSet<>()).asImmutable()
     }
 
     void addSkippedMember(GiveAway giveAway, Member member) {
-        Set<Member> members = skippedGiveWays[giveAway]
-        if (!members) {
-            members = new ConcurrentSkipListSet<Member>({ m1, m2 -> m1.id.compareTo(m2.id) })
-            skippedGiveWays.put(giveAway, members)
-        }
-        members.add(member)
+        skippedGiveWays.computeIfAbsent(giveAway) {
+            new ConcurrentSkipListSet<Member>({ m1, m2 -> m1.id.compareTo(m2.id) })
+        }.add(member)
     }
 }
